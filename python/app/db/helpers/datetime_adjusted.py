9688662bff5661159334f899043120dc0002b1ed
Tegar <tegar.sosi@gmail.com>
2025-04-11 17:58:14 +0200

    feat: ada event datas to db

--- python/app/db/helpers/datetime_adjusted.py
+++ python/app/db/helpers/datetime_adjusted.py
@@ -1,13 +1,13 @@
+import datetime
 from typing import Any
 
+import pandas as pd
 from tortoise import fields
 
 
 class DatetimeAdjustedField(fields.DatetimeField):
-    """Datetime adjusted field.
-
-    Ensure that the datetime is stored without timezone information
-    (timezone-naive)
+    """Datetime field that accepts Pandas Timestamps, ensures UTC
+    and strips tzinfos.
     """
 
     def to_db_value(self, value: Any, instance: Any) -> Any:
@@ -17,6 +17,13 @@ class DatetimeAdjustedField(fields.DatetimeField):
         :param instance: Instance of the model.
         :return: Datetime value without timezone information.
         """
-        if value:
-            return value.replace(tzinfo=None)
-        return value
+        if value is None:
+            return value
+
+        if isinstance(value, pd.Timestamp):
+            value = value.to_pydatetime()
+
+        if value.tzinfo is None:
+            value = value.replace(tzinfo=datetime.timezone.utc)
+
+        return value.replace(tzinfo=None)
--- python/app/fetchers/schedule.py
+++ python/app/fetchers/schedule.py
@@ -1,6 +1,48 @@
+import asyncio
 import fastf1 as ff1
 import pandas as pd
 from typing import List, Dict, Any
+
+from app.db.db import init_db, close_db
+from app.db.models.grand_prix import GrandPrix
+from app.db.models.session_types import SessionType
+from app.db.models.sessions import Session
+from app.enums.session_type import SessionType as SessionTypeEnum
+
+SESSION_TYPE_NAME_TO_CODE = {v.value: v.name for v in SessionTypeEnum}
+
+
+async def store_schedule_in_db(year: int, include_testing: bool) -> None:
+    """ Store the schedule in the database.
+
+    :param year: The year to store the schedule for.
+    :param include_testing: Whether to include testing events in the schedule.
+    """
+    await init_db()
+    schedule = get_schedule(year, include_testing)
+
+    for event in schedule:
+        gp, _ = await GrandPrix.get_or_create(
+            round=event["RoundNumber"],
+            event_name=event["EventName"],
+            location=event["Location"],
+            country=event["Country"],
+            year=year,
+        )
+
+        for session in event["sessions"]:
+            name = session["name"]
+            code = SESSION_TYPE_NAME_TO_CODE.get(name)
+            session_type, _ = await SessionType.get_or_create(
+                name=name,
+                code=code,
+            )
+            await Session.get_or_create(
+                grand_prix=gp,
+                session_type=session_type,
+                date_utc=session["date_utc"],
+            )
+    await close_db()
 
 
 def get_schedule(year: int, include_testing: bool) -> List[Dict[str, Any]]:
@@ -36,8 +78,9 @@ def get_schedule(year: int, include_testing: bool) -> List[Dict[str, Any]]:
 
 
 if __name__ == "__main__":
-    schedule = get_schedule(2025, False)
-    for event in schedule:
-        print(f"Round {event['RoundNumber']}: {event['EventName']} - {event['Country']}")
-        for session in event['sessions']:
-            print(f"  {session['name']}: {session['date_utc']}")
+    asyncio.run(store_schedule_in_db(2025, False))
+    # schedule = get_schedule(2025, False)
+    # for event in schedule:
+    #     print(f"Round {event['RoundNumber']}: {event['EventName']} - {event['Country']}")
+    #     for session in event['sessions']:
+    #         print(f"  {session['name']}: {session['date_utc']}")
